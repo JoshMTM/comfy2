e0300a584f6697a2fc868dbc0a13d4f8af0a1810
Josh <joshmitima@mbp-van-josh.home>
2023-04-07 12:34:22 +0200

    product list and filter added

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,14 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  const {filtered_products: products} = useFilterContext();
+  const {filtered_products: products, grid_view } = useFilterContext();
+  if(products.lengtg < 1){
+    return (
+    <h5 style={{textTransform: 'mone'}}>
+      Sorry no podcasts matches your search criteria.
+    </h5>
+    )
+  }
 
   return <GridView products={products}>Product List</GridView>
 }
