74a451994ce1671ce5bb80252a0989e91c503f9d
Josh <joshmitima@mbp-van-josh.home>
2023-06-01 22:04:42 +0200

    stripe settings

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <Auth0Provider
-    domain="dev-sas.eu.auth0.com"
-    clientId="0NdY1khtMKl1w6QLO2xdM8xGlQ55OxGa"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
